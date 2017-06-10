d8e85a0079d715c57db39f6ecfdd8af78d1a13cb
Nikola Bojic <Nikola Bojic>
2017-06-10 22:55:44 +0200

    cetvrti dan

---
+++ Index.js
@@ -0,0 +1,15 @@
+var i = 1;
+
+function slika() {
+	if(i == 1)
+	{
+		document.getElementById("slika").src = "images/treca.jpg";
+		i = 2;
+	}else
+	if(i == 2)
+	{
+		document.getElementById("slika").src = "images/cetvrta.jpg";
+		i = 3;
+	}else
+	if(i == 1)
+}
